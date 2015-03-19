657e18c5a1a20e09789adedf5a16c7478b030719
user <user@samsung>
2015-03-20 07:12:41 +0900

    디미베네

---
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,21 @@
+package org.dimigo.basic;
+
+public class Promotion {
+	public static void main(String[] args) {
+		
+		long month = 1700000;
+		int man = 3;
+		int jumpo = 1500;
+		
+		long year = (long)(month * 12 * man * jumpo);
+		
+		
+		System.out.println("<<디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 : " +  String.format("%,d",month));
+		System.out.println("점포 내 직원 수 : " + String.format("%,d",man));
+		System.out.println("연간 인건비 : " + String.format("%,d",year));
+		
+		
+		
+	}
+}
