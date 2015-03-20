ae2ab392f889abf47f5f3a879b28387837d26317
user <user@samsung>
2015-03-20 10:42:09 +0900

    3번째 실습 ( 넓이 비교 ) - 수정

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -12,13 +12,16 @@ public class Operator {
 		h = 5.4;
 		
 		double s2 = a*h;
-		double cha = s1 - s2;
 		
 		
 		System.out.println("<<도형 넓이 비교>>");
 		System.out.println("사다리꼴 넓이 : " + s1);
 		System.out.println("평행사변형 넓이 : " + s2);
-		System.out.println("사다리꼴이 평행사변형보다 " + cha + " 더 큽니다" );
+		if(s1 > s2)
+			System.out.println("사다리꼴이 평행사변형보다 " + (s1 - s2)+ " 더 큽니다" );
+		else
+			System.out.println("평행사변형이 사다리꼴보다 " + (s2 - s1)+ " 더 큽니다" );
+			
 		
 		
 
