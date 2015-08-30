4b5f0b3ccb4d2b7f891d116bafa196c6366df523
user <user@samsung>
2015-08-30 20:10:38 +0900

    스마트폰 .

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+public class SmartPhone {
+	
+	public String company;
+	public String model;
+	public int price;
+
+	public SmartPhone(){}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.company = company;
+		this.model = model;
+		this.price = price;
+	}
+	
+	public void turnOn()
+	{
+		System.out.println(this.model + "의 전원을 킵니다.");
+	}
+	public void turnOff()
+	{
+		System.out.println(this.model + "의 전원을 끕니다.");
+	}
+	public void pay()
+	{
+		
+	}
+	public void useSpecialFuntion(SmartPhone s)
+	{
+		if(s == null)
+		{
+			return;
+		}
+		else if(s instanceof IPhone){
+			IPhone a = (IPhone) s;
+			a.useAirDrop();
+		}
+		else{
+			Galaxy a = (Galaxy) s;
+			a.WirelessCharging();
+		}
+	}
+	@Override
+	public String toString() {
+		return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : "
+				+ price + "원";
+	}
+	
+
+}
