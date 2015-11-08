09fec61d3b27dcc1d720ef26daf4194cfd2e59fe
user <user@samsung>
2015-11-08 21:15:03 +0900

    10차

---
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * |_ Race
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ * 
+ * @author : 박도혁
+ * @version : 1.0
+ *
+ */
+public class Race2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("main thread start");
+//		System.out.println("main thread end");
+		
+		Thread r1 = new Thread( new Runner2("메이즈") );
+		Thread r2 = new Thread( new Runner2("러너") );
+		
+		r1.setPriority(Thread.MAX_PRIORITY);
+		r2.setPriority(Thread.MIN_PRIORITY);
+		
+		r1.start();
+		r2.start();
+
+	}
+
+}
